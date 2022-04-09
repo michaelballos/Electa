51b59a8d6841b16fac5dbd7eaff79a2b9fad7d4d
michaelballos <mdb3091207@gmail.com>
2022-04-09 13:52:34 -0500

    Add Initial
    About page

--- components/MainLinks.jsx
+++ components/MainLinks.jsx
@@ -1,14 +1,21 @@
-import React, { useCallback } from 'react';
-import { GitPullRequest, AlertCircle, Messages, Database, Home, ArrowForward } from 'tabler-icons-react';
-import { ThemeIcon, UnstyledButton, Group, Text } from '@mantine/core';
-import { useRouter } from 'next/router';
+import React, { useCallback } from 'react'
+import {
+  GitPullRequest,
+  AlertCircle,
+  Messages,
+  Database,
+  Home,
+  ArrowForward,
+} from 'tabler-icons-react'
+import { ThemeIcon, UnstyledButton, Group, Text } from '@mantine/core'
+import { useRouter } from 'next/router'
 
 function MainLink({ icon, color, label, url }) {
-  const { push: navigate } = useRouter();
-  
+  const { push: navigate } = useRouter()
+
   const handleNavigate = useCallback(() => {
-    navigate(url);
-  }, [navigate, url]);
+    navigate(url)
+  }, [navigate, url])
 
   return (
     <UnstyledButton
@@ -17,32 +24,64 @@ function MainLink({ icon, color, label, url }) {
         width: '100%',
         padding: theme.spacing.xs,
         borderRadius: theme.radius.sm,
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+        color:
+          theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
 
         '&:hover': {
           backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[6]
+              : theme.colors.gray[0],
         },
       })}
       onClick={handleNavigate}
     >
       <Group>
-        <ThemeIcon color={color} variant="light">
+        <ThemeIcon color={color} variant='light'>
           {icon}
         </ThemeIcon>
 
-        <Text size="sm">{label}</Text>
+        <Text size='sm'>{label}</Text>
       </Group>
     </UnstyledButton>
-  );
+  )
 }
 
 const data = [
   { icon: <Home size={16} />, color: 'blue', label: 'Home', url: '/' },
-  { icon: <ArrowForward size={16} />, color: 'teal', label: 'Login', url: '/login' },
-];
+  {
+    icon: <ArrowForward size={16} />,
+    color: 'teal',
+    label: 'Login',
+    url: '/login',
+  },
+  {
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        class='icon icon-tabler icon-tabler-info-circle'
+        width='17'
+        height='17'
+        viewBox='0 0 24 24'
+        stroke-width='1.5'
+        stroke='#00bfd8'
+        fill='none'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      >
+        <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+        <circle cx='12' cy='12' r='9' />
+        <line x1='12' y1='8' x2='12.01' y2='8' />
+        <polyline points='11 12 12 12 12 16 13 16' />
+      </svg>
+    ),
+    color: 'teal',
+    label: 'About',
+    url: '/about',
+  },
+]
 
 export function MainLinks() {
-  const links = data.map((link) => <MainLink {...link} key={link.label} />);
-  return <div>{links}</div>;
+  const links = data.map((link) => <MainLink {...link} key={link.label} />)
+  return <div>{links}</div>
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import '../styles/globals.css'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
-import Layout from '../components/Layout';
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
+import Layout from '../components/Layout'
 
-
-
-export default function App({ Component, pageProps}) {
+export default function App({ Component, pageProps }) {
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
@@ -17,27 +15,26 @@ export default function App({ Component, pageProps}) {
         <Head>
           <title>Page title</title>
           <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width'
           />
         </Head>
 
-          <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-              /** Put your mantine theme override here */
-              colorScheme: "light",
-            }}
-          >
-            <Hydrate state={pageProps.dehydratedState}>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </Hydrate>
-          </MantineProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: 'light',
+          }}
+        >
+          <Hydrate state={pageProps.dehydratedState}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Hydrate>
+        </MantineProvider>
       </QueryClientProvider>
     </>
-  );
+  )
 }
-
