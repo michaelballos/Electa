077e92dd2aa5ca94b5712f26e12d12abb84b08da
michaelballos <mdb3091207@gmail.com>
2022-04-09 11:15:47 -0500

    Add Register

--- pages/login.js
+++ pages/login.js
@@ -9,11 +9,12 @@ import {
   Button,
   Group,
   Stack,
+  Checkbox,
   useMantineTheme,
   Modal,
   TextInput,
 } from '@mantine/core'
-import { At } from 'tabler-icons-react'
+import { At, icon } from 'tabler-icons-react'
 import { useMutation } from 'react-query'
 
 const Login = () => {
@@ -102,8 +103,58 @@ const Login = () => {
                   }}
                   placeholder='Enter your magic phrase'
                   label='Password'
+                  icon={
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      class='icon icon-tabler icon-tabler-lock-off'
+                      width='15'
+                      height='15'
+                      viewBox='0 0 24 24'
+                      stroke-width='1.5'
+                      stroke='#2c3e50'
+                      fill='none'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    >
+                      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                      <line x1='3' y1='3' x2='21' y2='21' />
+                      <path d='M19 19a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2h4m4 0h2a2 2 0 0 1 2 2v2' />
+                      <circle cx='12' cy='16' r='1' />
+                      <path d='M8 11v-3m.712 -3.278a4 4 0 0 1 7.288 2.278v4' />
+                    </svg>
+                  }
                   required
                 />
+                <TextInput
+                  style={{
+                    paddingRight: '0.2rem',
+                    paddingLeft: '0.2rem',
+                  }}
+                  placeholder='Enter your magic phrase'
+                  label='Require Password'
+                  icon={
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      class='icon icon-tabler icon-tabler-lock-off'
+                      width='15'
+                      height='15'
+                      viewBox='0 0 24 24'
+                      stroke-width='1.5'
+                      stroke='#2c3e50'
+                      fill='none'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    >
+                      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                      <line x1='3' y1='3' x2='21' y2='21' />
+                      <path d='M19 19a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2h4m4 0h2a2 2 0 0 1 2 2v2' />
+                      <circle cx='12' cy='16' r='1' />
+                      <path d='M8 11v-3m.712 -3.278a4 4 0 0 1 7.288 2.278v4' />
+                    </svg>
+                  }
+                  required
+                />
+                <Checkbox size='xs' label='Save Magic Phrase' />
               </Stack>
             </Modal>
             <Group position='center'>
