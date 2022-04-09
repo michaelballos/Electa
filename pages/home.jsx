25c47d3c09e09463cbe829b49a2d38d008212f15
michaelballos <mdb3091207@gmail.com>
2022-04-09 09:59:39 -0500

    Partial Login

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,34 +1,43 @@
-import { Box, Divider, Title } from '@mantine/core';
-import { Group } from '@visx/group';
-import { Container, Stack } from 'react-bootstrap';
-import Graph from '../components/graph';
-import TableWrapper from '../components/TableWrapper';
+import { Box, Divider, Title } from '@mantine/core'
+import { Group } from '@visx/group'
+import { Container, Stack } from 'react-bootstrap'
+import Graph from '../components/Graph'
+import TableWrapper from '../components/TableWrapper'
 
 export default function HomePage() {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'nowrap',
-      }}
-    >
-      <Stack>
-        <Stack>
-          <Title order={1}>Electa API</Title>
-          <Title order={2}>Automate Delegation</Title>
-          <Divider />
-        </Stack>
-        <Container size="lg">
-          <TableWrapper />
-        </Container>
-      </Stack>
-      <Stack>
-        <Stack>
-          <Title order={3}>System Graph</Title>
-          <Graph />
-        </Stack>
-      </Stack>
-    </Box>
-  );
+    return ( <
+        Box sx = {
+            {
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'nowrap',
+            }
+        } >
+        <
+        Stack >
+        <
+        Stack >
+        <
+        Title order = { 1 } > Electa API < /Title> <
+        Title order = { 2 } > Automate Delegation < /Title> <
+        Divider / >
+        <
+        /Stack> <
+        Container size = 'lg' >
+        <
+        TableWrapper / >
+        <
+        /Container> <
+        /Stack> <
+        Stack >
+        <
+        Stack >
+        <
+        Title order = { 3 } > System Graph < /Title> <
+        Graph / >
+        <
+        /Stack> <
+        /Stack> <
+        /Box>
+    )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import styles from '../styles/login.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Head from 'next/head'
-import { Form, Button } from 'react-bootstrap'
+import {
+  Title,
+  Input,
+  Card,
+  Button,
+  Group,
+  Stack,
+  useMantineTheme,
+  Modal,
+  TextInput,
+} from '@mantine/core'
+import { At } from 'tabler-icons-react'
 import { useMutation } from 'react-query'
 
 const Login = () => {
+  const [opened, setOpened] = useState(false)
+
   const mutation = useMutation(
     'login',
     () => {
@@ -33,43 +47,70 @@ const Login = () => {
     }
   )
 
+  const theme = useMantineTheme()
+
+  const secondaryColor =
+    theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7]
+
   return (
     <>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Quicksand&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
       <div className={styles.loginContainer}>
-        <h1 className={styles.title}>Login</h1>
-        <Form className={'form'}>
-          <Form.Group
-            className={styles.emailContainer}
-            controlId='formBasicEmail'
-          >
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
-            <Form.Text className='neverShare'>
-              We&lsquo;ll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className='passContainer' controlId='formBasicPassword'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
-          </Form.Group>
-
-          <Form.Group className='passFormGroup' controlId='formBasicCheckbox'>
-            <Form.Check type='checkbox' label='Remember Password' />
-          </Form.Group>
-
-          <Button variant='primary' onClick={() => mutation.mutate()}>
-            Submit
-          </Button>
-        </Form>
+        <div style={{ width: 340, margin: 'auto' }}>
+          <Card shadow='sm' p='lg'>
+            <Modal
+              centered
+              opened={opened}
+              onClose={() => setOpened(false)}
+              title='Register with us!'
+            >
+              <Group>
+                <TextInput
+                  style={{
+                    padding: '.2rem',
+                  }}
+                  placeholder='Enter your first name'
+                  label='First Name'
+                  required
+                />
+                <TextInput
+                  style={{
+                    padding: '.2rem',
+                  }}
+                  placeholder='Enter your last name'
+                  label='Last Name'
+                  required
+                />
+              </Group>
+              <Stack>
+                <TextInput
+                  style={{
+                    paddingTop: '.8rem',
+                    paddingRight: '0.2rem',
+                    paddingLeft: '0.2rem',
+                  }}
+                  centered
+                  placeholder='Enter your email'
+                  error=''
+                  label='Email'
+                  icon={<At size={14} color={secondaryColor} />}
+                  required
+                />
+                <TextInput
+                  style={{
+                    paddingRight: '0.2rem',
+                    paddingLeft: '0.2rem',
+                  }}
+                  placeholder='Enter your magic phrase'
+                  label='Password'
+                  required
+                />
+              </Stack>
+            </Modal>
+            <Group position='center'>
+              <Button onClick={() => setOpened(true)}>Register</Button>
+            </Group>
+          </Card>
+        </div>
       </div>
     </>
   )
