1b1f9323c20e43ad259e915cfac90637b162db26
michaelballos <mdb3091207@gmail.com>
2022-04-09 01:19:30 -0500

    Add React Query

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import React from 'react'
 import '../styles/globals.css'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
