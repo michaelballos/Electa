7727613d227104eab9157376d758f01e6b54fbf3
Isaac Spiegel <isaacspiegel@gmail.com>
2022-04-08 23:33:54 -0500

    routing fixes

--- pages/Login.js
+++ pages/Login.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Login.module.css'
+import styles from '../styles/login.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Head from 'next/head'
 import { Form, Button } from 'react-bootstrap'
@@ -8,7 +8,7 @@ const Login = () => {
     <>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link
           href='https://fonts.googleapis.com/css2?family=Quicksand&display=swap'
           rel='stylesheet'
@@ -24,7 +24,7 @@ const Login = () => {
             <Form.Label>Email address</Form.Label>
             <Form.Control type='email' placeholder='Enter email' />
             <Form.Text className='neverShare'>
-              We'll never share your email with anyone else.
+              We&lsquo;ll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
 
