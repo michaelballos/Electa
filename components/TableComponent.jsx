35553b940bdeee5dff7718e3182167d7a53e5ec1
michaelballos <mdb3091207@gmail.com>
2022-04-09 06:20:04 -0500

    Add Table

--- components/TableComponent.jsx
+++ components/TableComponent.jsx
@@ -1,7 +1,7 @@
 import { Table } from '@mantine/core'
 import { useQuery } from 'react-query'
 
-const TableComponent = () => {
+function TableComponent() {
   const { data, status } = useQuery(
     'candidates',
     fetch('/api/candidates').then((res) => res.json())
@@ -9,53 +9,65 @@ const TableComponent = () => {
 
   const elements = [
     {
-      candidates: 'Candidates',
-      description: 'Description',
-      updatedAt: 'updatedAt',
-      createdAt: 'Carbon',
+      name: 6,
+      description: 12.011,
+      qualifications: 'C',
+      updatedAt: 'Carbon',
+      createdAt: 'Nitrogen',
+    },
+    {
+      name: 7,
+      description: 14.007,
+      qualifications: 'N',
+      updatedAt: 'Nitrogen',
+      createdAt: 'Nitrogen',
+    },
+    {
+      name: 39,
+      description: 88.906,
+      qualifications: 'Y',
+      updatedAt: 'Yttrium',
+      createdAt: 'Nitrogen',
+    },
+    {
+      name: 56,
+      description: 137.33,
+      qualifications: 'Ba',
+      updatedAt: 'Barium',
+      createdAt: 'Nitrogen',
+    },
+    {
+      name: 58,
+      description: 140.12,
+      qualifications: 'Ce',
+      updatedAt: 'Cerium',
+      createdAt: 'Nitrogen',
     },
   ]
 
   const rows = elements.map((element) => (
-    <tr key={element.candidates}>
-      <td> {element.description} </td> <td> {element.updatedAt} </td>{' '}
-      <td> {element.createdAt} </td>{' '}
+    <tr key={element.name}>
+      <td>{element.name}</td>
+      <td>{element.description}</td>
+      <td>{element.qualifications}</td>
+      <td>{element.updatedAt}</td>
+      <td>{element.createdAt}</td>
     </tr>
   ))
 
   return (
-    <>
-      <Table>
-        <thead>
-          <tr>
-            <th> Names </th>
-            <th> Description </th>
-            <th> updatedAt </th> <th> createdAt </th>{' '}
-          </tr>{' '}
-          <tbody> {rows} </tbody>{' '}
-        </thead>{' '}
-      </Table>{' '}
-      <button
-        style={{
-          float: 'right',
-          padding: '5px 10px',
-          margin: '15px 0',
-          borderRadius: '5px',
-        }}
-      >
-        add{' '}
-      </button>{' '}
-      <button
-        style={{
-          float: 'right',
-          padding: '5px 10px',
-          margin: '15px 25px',
-          borderRadius: '5px',
-        }}
-      >
-        delete{' '}
-      </button>{' '}
-    </>
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Qualifications</th>
+          <th>updatedAt</th>
+          <th>createdAt</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
   )
 }
 
