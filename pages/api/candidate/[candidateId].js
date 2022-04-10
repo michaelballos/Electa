1c5ffaa2ea4cf0f5789cde7f044cf5bb856f7246
michaelballos <mdb3091207@gmail.com>
2022-04-10 03:08:58 -0500

    Api Shit

--- pages/api/candidate/[candidateId].js
+++ pages/api/candidate/[candidateId].js
@@ -1,26 +1,29 @@
-import { deleteCandidate, getResourceById, updateCandidate } from '../mockDB'
+import {
+  deleteCandidate,
+  updateCandidate,
+  getResourceById,
+  updateQualification,
+  deleteRole,
+  getResourceById,
+  updateRole,
+} from '../mockDB'
 
 export default function handler(req, res) {
   const { candidateId } = req.query
+
   if (req.method === 'GET') {
-    const candidate = getResourceById('candidates', candidateId);
-    return res.status(200).json(
-      {
-        id: candidate.id,
-        name: candidate.name,
-        description: candidate.description,
-        delegations: candidate.roleIds,
-        endorsements: candidate.qualificationIds,
-      }
-    )
+    const role = getResourceById('candidate', candidateId)
+    return res.status(200).json({
+      id: candidate.id,
+      name: candidate.name,
+      description: candidate.description,
+      endorsements: candidate.candidateIds,
+      requirements: candidate.candidateIds,
+    })
   }
   if (req.method === 'PUT') {
     const { name, description } = req.body
-    const result = updateCandidate(
-      candidateId,
-      name,
-      description,
-    )
+    const result = updateRole(roleId, name, description)
     if (result) {
       return res.status(200).json({
         message: 'Successfully updated candidate',
@@ -30,15 +33,51 @@ export default function handler(req, res) {
       error: 'Failed to update candidate',
     })
   }
+
   if (req.method === 'DELETE') {
+    // Get the candidateIds associated with this qualification and delete
+    // the foreign key references to this qualification
+    const candidateId = getResourceById('candidate', candidateId).roleIds
+    candidateIds.forEach((candidateId) => {
+      const candidate = getResourceById('candidate', candidateId)
+      const newCandidateIds = candidate.candidateIds.filter(
+        (candidateId) => candidateId !== candidateId
+      )
+      updateCandidate(
+        candidate.Id,
+        candidate.name,
+        candidate.description,
+        newCandidateIds,
+        candidate.candidateIds
+      )
+    })
+
+    // Get the roleIds associated with this qualification and delete
+    // the foreign key references to this qualification
+    const candidateIds = getResourceById('candidate', candidateId).roleIds
+    candidateIds.forEach((candidateId) => {
+      const candidate = getResourceById('candidate', candidateId)
+      const newCandidateIds = candidate.candidateIds.filter(
+        (candidateId) => candidateId !== candidateId
+      )
+      updateCandidate(
+        candidateId,
+        candidate.name,
+        candidate.description,
+        candidate.roleIds,
+        newCandidateIds
+      )
+    })
+
     const result = deleteCandidate(candidateId)
+
     if (result) {
       return res.status(200).json({
         message: 'Successfully deleted candidate',
       })
     }
     return res.status(500).json({
-      error: 'Failed to delete candidate',
+      error: 'Failed to delete canidate',
     })
   }
 }
