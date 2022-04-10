d9478e558560c17c4a63e4e7234cc9060834288d
Isaac Spiegel <isaacspiegel@gmail.com>
2022-04-10 06:41:25 -0500

    Fix login

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { Card, Center } from "@mantine/core";
-import Register from "../components/Register";
-import LoginUI from "../components/LoginUI";
-import LoginButtons from "../components/LoginButtons";
-=======
 import React, { useState } from 'react'
 import { Card, Button, Center, Container, Title } from '@mantine/core'
 import Register from '../components/Register'
 import LoginUI from '../components/LoginUI'
 import LoginButtons from '../components/LoginButtons'
->>>>>>> cdf071d7734b0eef8fab7bc0fdb59a29b339b9fe
 
 /**
  * the login page and register form
@@ -20,38 +12,19 @@ const Login = () => {
   const [opened, setOpened] = useState(false);
 
   return (
-<<<<<<< HEAD
-    <Center
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-=======
     <Container
       sx={(theme) => ({
         backgroundColor: theme.colors.dark[7],
       })}
       fluid
       style={{ height: '100vh' }}
->>>>>>> cdf071d7734b0eef8fab7bc0fdb59a29b339b9fe
     >
       <Center
         style={{
-          width: 340,
-          margin: "auto",
+          width: '100%',
+          height: '100%',
         }}
       >
-<<<<<<< HEAD
-        <Card shadow="sm" p="lg">
-          <LoginUI />
-          <Register opened={opened} setOpened={setOpened} />
-          <LoginButtons opened={opened} setOpened={setOpened} />
-        </Card>
-      </div>
-    </Center>
-  );
-};
-=======
         <div
           style={{
             width: 340,
@@ -71,6 +44,5 @@ const Login = () => {
     </Container>
   )
 }
->>>>>>> cdf071d7734b0eef8fab7bc0fdb59a29b339b9fe
 
 export default Login;
