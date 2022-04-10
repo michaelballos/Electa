c30ac012d5231527904b5989e7431e89ce848261
Isaac Spiegel <isaacspiegel@gmail.com>
2022-04-10 05:21:42 -0500

    Create docs page

--- pages/Docs.js
+++ pages/Docs.js
@@ -1,10 +1,30 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page.</p>
-    </div>
-  )
-}
+import { ScrollArea } from '@mantine/core';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
-export default About
+import { createSwaggerSpec } from 'next-swagger-doc';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import { electaSpec } from '../api_specification/spec';
+
+const ApiDoc = ({ spec }) => {
+  return (
+    <ScrollArea style={{ height: '100%', backgroundColor: 'white' }} type="always">
+      <SwaggerUI spec={spec} />
+    </ScrollArea>
+  );
+};
+
+export const getStaticProps = async ctx => {
+  const spec = createSwaggerSpec({
+    definition: {
+      ...electaSpec,
+    },
+  });
+  return {
+    props: {
+      spec,
+    },
+  };
+};
+
+export default ApiDoc;
